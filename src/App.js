753263f9fe2f09355131485342ac9e4705a2154f
Gary Kang <garyzkang@gmail.com>
2024-01-18 20:19:37 -0500

    Add react-konva and drag n drop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,116 @@
+import React, { useEffect } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Stage, Layer, Image, Rect } from 'react-konva';
+import useImage from 'use-image';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+const URLImage = ({ image }) => {
+  const [img] = useImage(image.src);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Image
+      image={img}
+      x={image.x}
+      y={image.y}
+      // I will use offset to set origin to the center of the image
+      offsetX={img ? img.width / 2 : 0}
+      offsetY={img ? img.height / 2 : 0}
+    />
+  );
+};
+
+const App = () => {
+  const dragUrl = React.useRef();
+  const stageRef = React.useRef();
+  const [images, setImages] = React.useState([]);
+
+  useEffect(() => {
+    console.log("spagett!")
+  }, [])
+
+  return (
+    <div>
+      Drag this image on to the canvas stage:
+      <br />
+      <img
+        alt="React logo"
+        src={logo}
+        draggable="true"
+        onDragStart={(e) => {
+          dragUrl.current = e.target.src;
+        }}
+        // is it common to use inline styling nowadays?
+        style={{
+          height: 200,
+          width: 200,
+        }}
+      />
+      <div
+        onDrop={(e) => {
+          e.preventDefault();
+          // register event position
+          stageRef.current.setPointersPositions(e);
+          // add image
+          setImages(
+            images.concat([
+              {
+                ...stageRef.current.getPointerPosition(),
+                src: dragUrl.current,
+              },
+            ])
+          );
+        }}
+        onDragOver={(e) => e.preventDefault()}
+      >
+        <Stage
+          width={window.innerWidth}
+          height={window.innerHeight}
+          style={{ 
+            border: '1px solid grey',
+            backgroundColor: 'orange'
+           }}
+          ref={stageRef}
         >
-          Learn React
-        </a>
-      </header>
+          <Layer>
+            {images.map((image) => {
+              return <URLImage image={image} />;
+            })}
+          </Layer>
+        </Stage>
+      </div>
     </div>
   );
-}
+};
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
 
 export default App;
+
+
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
